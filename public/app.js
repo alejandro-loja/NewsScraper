f0328b55f7cd659d5cd08deb0ac0699f59834285
Alejandro Loja <alejandro.loja@gmail.com>
2019-01-20 20:48:12 -0500

    most logic down

---
+++ public/app.js
@@ -0,0 +1,78 @@
+// Grab the articles as a json
+$.getJSON("/articles", function (data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    var eachNews = data[i]
+  //  var eachArticle = $('#articles').append('<div class="each-article alert alert-warning alert-dismissible fade show" role="alert" data-id=' + eachNews._id + '><a class="alert-link" href="' + eachNews.link + '">' + eachNews.title + '</a><br />' + eachNews.desc + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+   var eachArticle = $('#articles').append('<div class="each-article alert alert-warning alert-dismissible fade show" role="alert" data-id=' + eachNews._id + '><a class="alert-link" href="' + eachNews.link + '">' + eachNews.title + '</a><br />' + eachNews.desc + '</div>');
+   // '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+  }
+});
+
+
+// Whenever someone clicks a p tag
+$(document).on("click", ".each-article", function () {
+  // Empty the notes from the note section
+  $("#notes").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function (data) {
+      console.log(data);
+      // The title of the article
+      $("#notes").append("<h5>" + data.title + "</h5>");
+      // An input to enter a new title
+      $("#notes").append("<input class='m-1 form-control' id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea class='form-control m-1' id='bodyinput' name='body' rows='5'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button class='btn btn-warning' data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      // A add button to delete comment
+      $("#notes").append('</br><button class="btn btn-light" type="button">Clear Comment</button>');
+
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function () {
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      // Value taken from title input
+      title: $("#titleinput").val(),
+      // Value taken from note textarea
+      body: $("#bodyinput").val()
+    }
+  })
+    // With that done
+    .then(function (data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+    });
+
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
--- server.js
+++ server.js
@@ -31,27 +31,31 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
+
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
-    axios.get("http://www.echojs.com/").then(function(response) {
+    axios.get("https://www.cnet.com/news/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
+      $("section.dontMissRight").children().each(function (i, element) {
         // Save an empty result object
         var result = {};
-  
+        var block = $(element).children($("div", ".asset"));
+        var title = block.find('h4').text().trim();
+        var link = block.find('a').attr('href');
+        link = 'https://www.cnet.com' + link
+        var desc = block.find('.dek').text().trim();
+
         // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
+        result.title = title;
+        result.desc = desc;
+        result.link = link;
   
         // Create a new Article using the `result` object built from scraping
+        if(!title == '') {
         db.Article.create(result)
           .then(function(dbArticle) {
             // View the added result in the console
@@ -61,6 +65,7 @@ app.get("/scrape", function(req, res) {
             // If an error occurred, log it
             console.log(err);
           });
+        }
       });
   
       // Send a message to the client
